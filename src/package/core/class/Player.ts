e1752cea48f16d7b5b6136cf4d6f26c55b3263e7
sweshelo <sweshelo@gmail.com>
2025-03-17 18:29:28 +0900

    idから対象を特定する関数を追加

--- src/package/core/class/Player.ts
+++ src/package/core/class/Player.ts
@@ -1,3 +1,4 @@
+import { config } from "../../../config"
 import type { Action } from "./action"
 import type { Atom } from "./card/Atom"
 import type { Card } from "./card/Card"
@@ -6,6 +7,15 @@ import { Unit } from "./card/Unit"
 export interface PlayerAction {
   action: Action
   stack?: unknown
+}
+
+export interface FindResult {
+  result: boolean
+  card?: Card
+  place?: {
+    name: string
+    ref: Array<Card>
+  }
 }
 
 export class Player {
@@ -23,6 +33,42 @@ export class Player {
     this.field = []
   }
 
+  // プレイヤー領域からカードを探す
+  find(target: Card): FindResult {
+    const onDeck = this.deck.find(({ id }) => id === target.id)
+    if (onDeck) return ({
+      result: true,
+      card: onDeck,
+      place: {
+        name: 'deck',
+        ref: this.deck
+      }
+    })
+
+    const onHand = this.hand.find(({ id }) => id === target.id)
+    if (onHand) return ({
+      result: true,
+      card: onHand,
+      place: {
+        name: 'hand',
+        ref: this.hand
+      }
+    })
+
+    // 本当に必要?
+    const onField = this.field.find(({ id }) => id === target.id)
+    if (onField) return ({
+      result: true,
+      card: onField,
+      place: {
+        name: 'field',
+        ref: this.field
+      }
+    })
+
+    return { result: false }
+  }
+
   // カードを引く
   draw(): PlayerAction | null {
     if (this.deck.length > 0) {
@@ -33,6 +79,22 @@ export class Player {
           role: 'system',
           type: 'draw',
           source: source,
+        }
+      }
+    } else {
+      return null
+    }
+  }
+
+  // ドライブ
+  drive(target: Unit) {
+    if (this.field.length < config.game.player.max.field) {
+      this.field.push(target);
+      return {
+        action: {
+          role: 'user',
+          type: 'drive',
+          source: target
         }
       }
     } else {
