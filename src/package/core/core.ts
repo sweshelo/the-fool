ca89575872cbee5b55a1a4811a43b142b7cfb08c
sweshelo <sweshelo@gmail.com>
2025-03-27 22:58:01 +0900

    debug draw

--- src/package/core/core.ts
+++ src/package/core/core.ts
@@ -1,16 +1,20 @@
 import type { Message } from "@/submodule/suit/types/message/message";
 import type { Player } from "./class/Player";
 import { config } from "../../config";
+import type { DebugDrawPayload } from "@/submodule/suit/types";
+import type { Room } from "../server/room/room";
 
 export class Core {
   id: string;
   players: Player[];
   round: number = 0;
   turn: number = 0;
+  room: Room;
 
-  constructor() {
+  constructor(room: Room) {
     this.id = crypto.randomUUID()
     this.players = [];
+    this.room = room
   }
 
   entry(player: Player) {
@@ -40,5 +44,16 @@ export class Core {
 
   handleMessage(message: Message) {
     console.log('passed message to Core : type<%s>', message.action.type)
+    switch(message.payload.type){
+      case 'DebugDraw': {
+        const payload: DebugDrawPayload = message.payload
+        const target = this.players.find(player => player.id === payload.player)
+        if(target){
+          target.draw()
+          console.log('success draw')
+          this.room.sync()
+        }
+      }
+    }
   }
 }
