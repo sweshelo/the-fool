901e63a3b8279ee60dae33fb3195563de2078d2f
Sweshelo <sweshelo@gmail.com>
2025-04-29 12:49:40 +0900

    Update src/database/effects/cards/PR-179.ts

--- src/database/effects/cards/PR-179.ts
+++ src/database/effects/cards/PR-179.ts
@@ -9,7 +9,7 @@ export const effects: CardEffects = {
     const isBouncedMoreThan10Cards = owner.trash.length >= 10;
     await System.show(
       stack,
-      '飛天矛槍演舞＆秩序の盾',
+      '飛天矛槍演舞',
       `お互いの捨札をデッキに戻す\n${isBouncedMoreThan10Cards ? 'カードを1枚引く' : ''}\n【天使】に【秩序の盾】を与える\n効果によるダメージを受けない`
     );
 
