9fafe61cf256b103b774abd45e593db5ede4cb11
sweshelo <sweshelo@gmail.com>
2025-05-05 13:38:49 +0900

    復刻版カードはオリジナルをimportするだけに

--- src/database/effects/cards/SP-016.ts
+++ src/database/effects/cards/SP-016.ts
@@ -1,13 +1,2 @@
-import { EffectTemplate, System } from '..';
-import { Color } from '@/submodule/suit/constant/color';
-import type { StackWithCard } from '../classes/types';
-
-export const effects = {
-  onDriveSelf: async (stack: StackWithCard) => {
-    await System.show(stack, '援軍／緑', '緑属性ユニットを1枚引く');
-    EffectTemplate.reinforcements(stack, stack.processing.owner, {
-      color: Color.GREEN,
-      type: ['unit', 'advanced_unit'],
-    });
-  },
-};
+import { effects } from './1-1-018';
+export { effects };
