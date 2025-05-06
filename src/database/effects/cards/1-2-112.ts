9013e6f5a8dd90d99db42ac372aaf594e108f043
sweshelo <sweshelo@gmail.com>
2025-05-06 18:18:59 +0900

    カードを追加

---
+++ src/database/effects/cards/1-2-112.ts
@@ -0,0 +1,54 @@
+import { Unit } from '@/package/core/class/card';
+import { Effect, EffectHelper, System } from '..';
+import type { CardEffects, StackWithCard } from '../classes/types';
+
+export const effects: CardEffects = {
+  onPlayerAttackSelf: async (stack: StackWithCard): Promise<void> => {
+    await System.show(stack, '無我夢中の鍛錬', 'レベル+1');
+    Effect.clock(stack, stack.processing, stack.processing as Unit, 1);
+  },
+
+  onOverclockSelf: async (stack: StackWithCard): Promise<void> => {
+    const candidate = EffectHelper.candidate(
+      stack.core,
+      unit => unit.owner.id !== stack.processing.owner.id
+    );
+
+    if (candidate.length > 0) {
+      await System.show(stack, '白夜の封剣', '行動権を消費\n【呪縛】を付与');
+      const [unitId] = await System.prompt(stack, stack.processing.owner.id, {
+        type: 'unit',
+        title: '行動権を消費し【呪縛】を与えるユニットを選択',
+        items: candidate,
+      });
+
+      const target = candidate.find(unit => unit.id === unitId);
+      if (target) {
+        Effect.activate(stack, stack.processing, target, false);
+        Effect.keyword(stack, stack.processing, target, '呪縛');
+      }
+    }
+  },
+
+  fieldEffect: (stack: StackWithCard) => {
+    if (
+      stack.processing.delta.some(
+        delta =>
+          delta.source?.unit === stack.processing.id && delta.source.effectCode === '神速の一閃'
+      )
+    ) {
+      if (stack.processing.lv !== 1)
+        stack.processing.delta = stack.processing.delta.filter(
+          delta =>
+            delta.source?.unit === stack.processing.id && delta.source.effectCode === '神速の一閃'
+        );
+    } else {
+      if (stack.processing.lv === 1 && stack.processing instanceof Unit) {
+        Effect.keyword(stack, stack.processing, stack.processing, '次元干渉', {
+          cost: 0,
+          source: { unit: stack.processing.id, effectCode: '次元干渉' },
+        });
+      }
+    }
+  },
+};
---
+++ src/database/effects/cards/2-0-006.ts
@@ -0,0 +1,62 @@
+import type { Unit } from '@/package/core/class/card';
+import { Effect, EffectHelper, System } from '..';
+import type { CardEffects, StackWithCard } from '../classes/types';
+
+export const effects: CardEffects = {
+  // 自身が召喚された時に発動する効果を記述
+  onDriveSelf: async (stack: StackWithCard): Promise<void> => {
+    await System.show(stack, '胎動＆固着', '手札に戻らない\nコスト3以下に【狂戦士】を与える');
+    Effect.keyword(stack, stack.processing, stack.processing as Unit, '固着');
+  },
+
+  onWin: async (stack: StackWithCard): Promise<void> => {
+    const candidate = EffectHelper.candidate(
+      stack.core,
+      unit => unit.owner.id !== stack.processing.owner.id
+    );
+
+    if (candidate.length > 0) {
+      await System.show(stack, '剪定', '3000ダメージ');
+      const [unitId] = await System.prompt(stack, stack.processing.owner.id, {
+        type: 'unit',
+        title: 'ダメージを与えるユニットを選択',
+        items: candidate,
+      });
+
+      const target = candidate.find(unit => unit.id === unitId);
+      if (target) {
+        Effect.damage(stack, stack.processing, target, 3000, 'effect');
+      }
+    }
+  },
+
+  onClockup: async (stack: StackWithCard): Promise<void> => {
+    const targets = stack.processing.owner.opponent.field;
+
+    if (targets.length > 0 && stack.processing.lv === 3) {
+      await System.show(stack, '剪定', '敵全体に[対戦相手の手札×2000]ダメージ');
+      targets.forEach(unit =>
+        Effect.damage(
+          stack,
+          stack.processing,
+          unit,
+          stack.processing.owner.opponent.hand.length * 2000
+        )
+      );
+      Effect.clock(stack, stack.processing, stack.processing as Unit, -2);
+    }
+  },
+
+  fieldEffect: (stack: StackWithCard) => {
+    stack.processing.owner.opponent.field.forEach(unit => {
+      if (
+        !unit.delta.some(
+          delta => delta.source?.unit === stack.processing.id && unit.catalog.cost <= 3
+        )
+      )
+        Effect.keyword(stack, stack.processing, unit, '狂戦士', {
+          source: { unit: stack.processing.id },
+        });
+    });
+  },
+};
