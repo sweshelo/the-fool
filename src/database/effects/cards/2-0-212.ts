3f60b1982bf2a8e21a0292833c5d4e6b8eccd83c
sweshelo <sweshelo@gmail.com>
2025-05-10 12:04:04 +0900

    #71 のレビュー指摘内容読み漏れ修正

--- src/database/effects/cards/2-0-212.ts
+++ src/database/effects/cards/2-0-212.ts
@@ -2,41 +2,28 @@ import { Evolve, Unit } from '@/package/core/class/card';
 import { Effect, EffectHelper, System } from '..';
 import type { CardEffects, StackWithCard } from '../classes/types';
 
+const mainEffect = async (stack: StackWithCard<Unit>) => {
+  const candidate = stack.processing.owner.trash.filter(
+    card =>
+      card instanceof Unit &&
+      !(card instanceof Evolve) &&
+      card.catalog.cost <= 3 &&
+      card.catalog.species?.includes('侍')
+  ) as Unit[];
+
+  if (stack.processing.owner.field.length <= 4 && candidate.length > 0) {
+    await System.show(stack, '戦友と共に', '捨札から【侍】を【特殊召喚】');
+    const [target] = EffectHelper.random(candidate);
+    await Effect.summon(stack, stack.processing, target!);
+  }
+};
+
 export const effects: CardEffects = {
   // ■戦友と共に
   // このユニットがフィールドに出た時、また効果によって破壊された時、あなたのフィールドのユニットが4体以下の場合、
   // あなたの捨札にある進化ユニット以外のコスト3以下の【侍】ユニットを1体【特殊召喚】する。
-  onDriveSelf: async (stack: StackWithCard<Unit>): Promise<void> => {
-    const candidate = stack.processing.owner.trash.filter(
-      card =>
-        card instanceof Unit &&
-        !(card instanceof Evolve) &&
-        card.catalog.cost <= 3 &&
-        card.catalog.species?.includes('侍')
-    ) as Unit[];
-
-    if (stack.processing.owner.field.length <= 4 && candidate.length > 0) {
-      await System.show(stack, '戦友と共に', '捨札から【侍】を【特殊召喚】');
-      const [target] = EffectHelper.random(candidate);
-      await Effect.summon(stack, stack.processing, target!);
-    }
-  },
-
-  onBreakSelf: async (stack: StackWithCard<Unit>): Promise<void> => {
-    const candidate = stack.processing.owner.trash.filter(
-      card =>
-        card instanceof Unit &&
-        !(card instanceof Evolve) &&
-        card.catalog.cost <= 3 &&
-        card.catalog.species?.includes('侍')
-    ) as Unit[];
-
-    if (stack.processing.owner.field.length <= 4 && candidate.length > 0) {
-      await System.show(stack, '戦友と共に', '捨札から【侍】を【特殊召喚】');
-      const [target] = EffectHelper.random(candidate);
-      await Effect.summon(stack, stack.processing, target!);
-    }
-  },
+  onDriveSelf: mainEffect,
+  onBreakSelf: mainEffect,
 
   // ■壬生の鬼
   // このユニットが戦闘した時、それがアタック中であなたのフィールドに【侍】ユニットが3体以上いる場合、
