91246572ba9aea4c61cb32d7c8e8990c9c09d047
sweshelo <sweshelo@gmail.com>
2025-05-20 23:10:23 +0900

    as 使用箇所 修正

--- src/database/effects/cards/1-1-006.ts
+++ src/database/effects/cards/1-1-006.ts
@@ -1,17 +1,17 @@
-import type { Unit } from '@/package/core/class/card';
+import { Unit } from '@/package/core/class/card';
 import { Effect, System } from '..';
 import type { CardEffects, StackWithCard } from '../classes/types';
 
 export const effects: CardEffects = {
   // 自身が召喚された時に発動する効果を記述
-  onDriveSelf: async (stack: StackWithCard): Promise<void> => {
+  onDriveSelf: async (stack: StackWithCard<Unit>): Promise<void> => {
     await System.show(
       stack,
       '煉獄の判決＆消滅効果耐性',
       '敵全体のBP-1000\n対戦相手の効果によって消滅しない'
     );
-    Effect.keyword(stack, stack.processing, stack.processing as Unit, '消滅効果耐性');
-    Effect.speedMove(stack, stack.processing as Unit);
+    Effect.keyword(stack, stack.processing, stack.processing, '消滅効果耐性');
+    Effect.speedMove(stack, stack.processing);
   },
 
   fieldEffect: (stack: StackWithCard) => {
