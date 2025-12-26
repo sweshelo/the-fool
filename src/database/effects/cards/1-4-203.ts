a773b37aacfaffbd7b274ea6315f321075a62cac
sweshelo <sweshelo@gmail.com>
2025-12-27 00:35:04 +0900

    カードを追加

---
+++ src/database/effects/cards/1-4-203.ts
@@ -0,0 +1,62 @@
+import { Effect, EffectHelper, EffectTemplate, System } from '..';
+import type { CardEffects, StackWithCard } from '../classes/types';
+import { Unit } from '@/package/core/class/card';
+
+const balsamDance = async (stack: StackWithCard, hideMessage = false) => {
+  if (
+    !(stack.target instanceof Unit) ||
+    !stack.processing.owner.field.find(unit => unit.id == stack.target?.id)
+  )
+    return;
+
+  // 相手フィールドに選択可能なユニットが存在するか
+  const owner = stack.processing.owner;
+  const candidate = EffectHelper.candidate(
+    stack.core,
+    (unit: Unit) => unit.owner.id !== owner.id,
+    stack.processing.owner
+  );
+
+  if (!hideMessage) await System.show(stack, '鳳仙花の舞', '[【舞姫】×1000]ダメージ');
+  const [target] = await EffectHelper.selectUnit(
+    stack,
+    stack.processing.owner,
+    candidate,
+    'ダメージを与えるユニットを選択して下さい'
+  );
+
+  Effect.damage(
+    stack,
+    stack.processing,
+    target,
+    stack.processing.owner.field.filter(unit => unit.catalog.species?.includes('舞姫')).length *
+      1000,
+    'effect'
+  );
+};
+
+export const effects: CardEffects = {
+  // 自身が召喚された時に発動する効果を記述
+  onDriveSelf: async (stack: StackWithCard): Promise<void> => {
+    const candidate = EffectHelper.candidate(
+      stack.core,
+      (unit: Unit) => unit.owner.id !== stack.processing.owner.id,
+      stack.processing.owner
+    );
+
+    if (candidate.length > 0) {
+      await System.show(
+        stack,
+        '鳳仙花の舞＆援軍／舞姫',
+        '【舞姫】を1枚引く\n[【舞姫】×1000]ダメージ'
+      );
+      EffectTemplate.reinforcements(stack, stack.processing.owner, { species: '舞姫' });
+      await balsamDance(stack, true);
+    } else {
+      await System.show(stack, '援軍／舞姫', '【舞姫】を1枚引く');
+      EffectTemplate.reinforcements(stack, stack.processing.owner, { species: '舞姫' });
+    }
+  },
+
+  onDrive: balsamDance,
+};
---
+++ src/database/effects/cards/PR-155.ts
@@ -0,0 +1,24 @@
+import { Effect, EffectHelper, EffectTemplate, System } from '..';
+import type { CardEffects, StackWithCard } from '../classes/types';
+
+const replaceMagick = async (stack: StackWithCard) => {
+  if (stack.source.id === stack.processing.owner.id && stack.processing.owner.hand.length > 0) {
+    await System.show(stack, 'リプレイスマジック', '手札を1枚捨てる\nカードを1枚引く\n紫ゲージ+1');
+    const [target] = await EffectHelper.selectCard(
+      stack,
+      stack.processing.owner,
+      stack.processing.owner.hand,
+      '捨てるカードを選んで下さい'
+    );
+    Effect.handes(stack, stack.processing, target);
+    EffectTemplate.draw(stack.processing.owner, stack.core);
+    Effect.modifyPurple(stack, stack.processing, stack.processing.owner, 1);
+  }
+};
+
+export const effects: CardEffects = {
+  onDriveSelf: replaceMagick,
+  onDrive: async (stack: StackWithCard) => {
+    if (stack.processing.id !== stack.source.id) replaceMagick(stack);
+  },
+};
