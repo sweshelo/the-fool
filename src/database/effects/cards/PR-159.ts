951603a55b39dea29f98de89fad74d59ceb6c678
sweshelo <sweshelo@gmail.com>
2025-12-28 11:19:51 +0900

    求愛のダンス: PA効果を実装

--- src/database/effects/cards/PR-159.ts
+++ src/database/effects/cards/PR-159.ts
@@ -1,5 +1,7 @@
+import { Unit } from '@/package/core/class/card';
 import { EffectTemplate, System } from '..';
 import type { CardEffects, StackWithCard } from '../classes/types';
+import { Color } from '@/submodule/suit/constant/color';
 
 export const effects: CardEffects = {
   checkDrive: (stack: StackWithCard): boolean => {
@@ -9,5 +11,18 @@ export const effects: CardEffects = {
   onDrive: async (stack: StackWithCard): Promise<void> => {
     await System.show(stack, '求愛のダンス', 'トリガーカードを1枚引く');
     EffectTemplate.reinforcements(stack, stack.processing.owner, { type: ['trigger'] });
+  },
+
+  checkPlayerAttack: (stack: StackWithCard) => {
+    return (
+      stack.source instanceof Unit &&
+      stack.source.owner.id === stack.processing.owner.id &&
+      stack.source.catalog.color === Color.BLUE
+    );
+  },
+
+  onPlayerAttack: async (stack: StackWithCard) => {
+    await System.show(stack, '求愛のダンス', '捨札から1枚選んで回収');
+    await EffectTemplate.revive(stack, 1);
   },
 };
