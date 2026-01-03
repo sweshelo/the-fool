1497aaa9cc5ded23f8db5eac930ba65bf7a83a33
sweshelo <sweshelo@gmail.com>
2026-01-03 18:51:55 +0900

    handes -> move

--- src/database/effects/jokers/the-devil/guilty-ash.ts
+++ src/database/effects/jokers/the-devil/guilty-ash.ts
@@ -15,7 +15,7 @@ export const effects: CardEffects = {
 
     if (opponent.trigger.length === 0) return;
 
-    await System.show(stack, 'ギルティアッシュ', 'トリガー公開\n1枚破壊');
+    await System.show(stack, 'ギルティアッシュ', 'トリガーゾーンを公開\n1枚選んで破壊');
 
     // 対戦相手はトリガーゾーンを公開する
     // プレイヤーがトリガーゾーンのカードを1枚選んで破壊する
@@ -26,6 +26,6 @@ export const effects: CardEffects = {
       '破壊するカードを選択'
     );
 
-    Effect.handes(stack, stack.processing, target);
+    Effect.move(stack, stack.processing, target, 'trash');
   },
 };
