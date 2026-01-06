2e60cc7000619b1846d23789573cfbf0101f8c28
totoneko <32995138+totoneko@users.noreply.github.com>
2026-01-06 19:09:53 +0900

    カード追加

---
+++ src/database/effects/cards/1-2-007.ts
@@ -0,0 +1,16 @@
+import { Effect, System } from '..';
+import type { CardEffects, StackWithCard } from '../classes/types';
+import { Unit } from '@/package/core/class/card';
+
+export const effects: CardEffects = {
+  //■ダークソウル
+  //このユニットがアタックした時、このユニットの基本BPを+［あなたの捨札の数×500］する。
+
+  onAttackSelf: async (stack: StackWithCard<Unit>) => {
+    const self = stack.processing;
+
+    await System.show(stack, 'ダークソウル', '基本BP+捨札の数×500');
+
+    Effect.modifyBP(stack, self, self, self.owner.trash.length * 500, { isBaseBP: true });
+  },
+};
