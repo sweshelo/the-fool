b0b6a395624ffad85a99ea7256c05664031d6fc6
sweshelo <sweshelo@gmail.com>
2026-01-10 20:00:39 +0900

    実装漏れを修正

--- src/database/effects/cards/1-0-114.ts
+++ src/database/effects/cards/1-0-114.ts
@@ -1,9 +1,13 @@
 import type { Unit } from '@/package/core/class/card';
 import type { CardEffects, StackWithCard } from '../classes/types';
 import { System } from '../classes/system';
+import { Effect } from '../classes/effect';
 
 export const effects: CardEffects = {
   onOverclockSelf: async (stack: StackWithCard<Unit>) => {
     await System.show(stack, '天変地異', '全てのユニットを手札に戻す');
+    stack.core.players
+      .flatMap(player => player.field)
+      .forEach(unit => Effect.bounce(stack, stack.processing, unit, 'hand'));
   },
 };
