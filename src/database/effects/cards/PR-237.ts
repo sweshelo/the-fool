95bc08bf46aaab9d07eaa165881d64848ef1fc0c
sweshelo <sweshelo@gmail.com>
2026-01-12 00:07:17 +0900

    表示がないのを修正

--- src/database/effects/cards/PR-237.ts
+++ src/database/effects/cards/PR-237.ts
@@ -1,11 +1,13 @@
 import { Effect } from '../classes/effect';
 import { EffectHelper } from '../classes/helper';
+import { System } from '../classes/system';
 import type { CardEffects, StackWithCard } from '../classes/types';
 
 export const effects: CardEffects = {
   // ユニットがフィールドに出た時: 所有者を問わない
   checkDrive: stack => stack.core.players.flatMap(player => player.field).length > 0,
   onDrive: async (stack: StackWithCard) => {
+    await System.show(stack, 'キングオブキングス', '1000ダメージ×5回');
     EffectHelper.repeat(5, () => {
       const [target] = EffectHelper.random(stack.core.players.flatMap(player => player.field));
       if (target) Effect.damage(stack, stack.processing, target, 1000);
