cea9724ae56c8d7cefa40e6e1e5746cbfa2e8e03
sweshelo <sweshelo@gmail.com>
2026-01-12 00:03:21 +0900

    対象が逆

--- src/database/effects/cards/1-2-148.ts
+++ src/database/effects/cards/1-2-148.ts
@@ -4,7 +4,7 @@ import { System } from '../classes/system';
 import { Effect } from '../classes/effect';
 
 export const effects: CardEffects = {
-  checkBattle: stack => stack.processing.owner.trigger.length > 0,
+  checkBattle: stack => stack.processing.owner.opponent.trigger.length > 0,
   onBattle: async (stack: StackWithCard) => {
     // 戦闘中の自ユニットを特定
     const [target] = [stack.source, stack.target].filter(
