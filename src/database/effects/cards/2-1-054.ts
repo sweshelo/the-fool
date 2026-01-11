6c95148290843f87cd7a4400fe4656164ed59cfa
sweshelo <sweshelo@gmail.com>
2026-01-11 17:07:49 +0900

    複数回使用可能なインターセプトに対応

---
+++ src/database/effects/cards/2-1-054.ts
@@ -0,0 +1,22 @@
+import { Unit } from '@/package/core/class/card';
+import { Effect } from '../classes/effect';
+import { System } from '../classes/system';
+import type { CardEffects, StackWithCard } from '../classes/types';
+
+const condition = (stack: StackWithCard) =>
+  stack.processing.owner.id !== stack.source.id && stack.processing.owner.field.length <= 4;
+const effect = async (stack: StackWithCard) => {
+  await System.show(stack, 'クンフーマスター', '[クンフーマスター] を【特殊召喚】');
+  await Effect.summon(
+    stack,
+    stack.processing,
+    new Unit(stack.processing.owner, 'クンフーマスター')
+  );
+};
+
+export const effects: CardEffects = {
+  checkTurnEnd: condition,
+  checkAttack: condition,
+  onTurnEnd: effect,
+  onAttack: effect,
+};
--- src/package/core/class/card/Intercept.ts
+++ src/package/core/class/card/Intercept.ts
@@ -1,18 +1,38 @@
 import type { ICard } from '@/submodule/suit/types';
 import { Card } from './Card';
-import type { Delta } from '../delta';
+import { Delta } from '../delta';
 import type { Player } from '../Player';
 
 export class Intercept extends Card implements ICard {
+  #originalRemain: number;
+  remain: number;
+  revealed: boolean = false;
+
+  constructor(owner: Player, catalogId: string) {
+    super(owner, catalogId);
+
+    const [match, count] =
+      this.catalog.ability?.match(/このインターセプト(?:カード)?は(\d+)回使用すると捨札にいく/) ??
+      [];
+    console.log(this.catalog.name, count);
+    this.#originalRemain = match && count ? parseInt(count, 10) : 1;
+    this.remain = this.#originalRemain;
+  }
+
   clone(owner: Player): Intercept {
     const card = new Intercept(owner, this.catalogId);
-    card.delta = this.delta?.map<Delta>(buff => ({
-      ...buff,
-      checkExpire: buff.checkExpire.bind(card),
-      event: undefined,
-      source: undefined,
-    }));
+    card.delta = this.delta?.map<Delta>(delta => {
+      return new Delta(delta.effect, {
+        ...delta,
+      });
+    });
     card.lv = this.lv;
     return card;
+  }
+
+  reset(): void {
+    super.reset();
+    this.revealed = false;
+    this.remain = this.#originalRemain;
   }
 }
