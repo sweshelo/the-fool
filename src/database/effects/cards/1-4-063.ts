67940a4e32e6d714e14aa1fa73988900c959201b
inutiyohondam-cmd <inutiyohondam@gmail.com>
2026-01-15 20:55:27 +0900

    Update 1-4-063.ts

--- src/database/effects/cards/1-4-063.ts
+++ src/database/effects/cards/1-4-063.ts
@@ -1,7 +1,17 @@
+import { Color } from '@/submodule/suit/constant/color';
 import { EffectHelper, EffectTemplate, System } from '..';
 import type { CardEffects, StackWithCard } from '../classes/types';
+import { Unit } from '@/package/core/class/card/Unit';
 
 export const effects: CardEffects = {
+  checkDrive: (stack: StackWithCard): boolean => {
+    return (
+      stack.processing.owner.id === stack.source.id &&
+      stack.target instanceof Unit &&
+      stack.target.catalog.color === Color.BLUE
+    );
+  },
+
   // あなたのユニットがフィールドに出た時
   onDrive: async (stack: StackWithCard): Promise<void> => {
     const owner = stack.processing.owner;
@@ -12,14 +22,14 @@ export const effects: CardEffects = {
       await System.show(
         stack,
         '黙想の薔薇',
-        '対戦相手のフィールドにいる【ウィルス】ユニットを除外\n＜ウィルス・黙＞を【特殊召喚】'
+        '対戦相手のフィールドにいる【ウィルス】ユニットを除外\n＜ウィルス・黙＞を【特殊召喚】\nインターセプトカードを1枚引く'
       );
 
       // virusInjectはウィルスの除外と特殊召喚を両方処理する
       await EffectTemplate.virusInject(stack, opponent, '＜ウィルス・黙＞');
 
       // ウィルスを特殊召喚できた場合、インターセプトカードを1枚引く
-      await System.show(stack, '黙想の薔薇', 'インターセプトカードを1枚引く');
+
       EffectTemplate.reinforcements(stack, owner, {
         type: ['intercept'],
       });
