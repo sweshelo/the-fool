2995be0857c09fb11bb45f2cafec0c60ba21da23
dalproc <username@users.noreply.github.com>
2026-01-17 23:15:52 +0900

    [＜ウィルス・攻＞] 相手ユニット攻撃時に効果が発動するよう修正

--- src/game-data/effects/cards/＜ウィルス・攻＞.ts
+++ src/game-data/effects/cards/＜ウィルス・攻＞.ts
@@ -6,9 +6,9 @@ export const effects: CardEffects = {
   onAttack: async (stack: StackWithCard<Unit>): Promise<void> => {
     const isOpponentUnitAttacked = stack.source.id !== stack.processing.owner.id;
 
-    if (isOpponentUnitAttacked && stack.source instanceof Unit) {
+    if (isOpponentUnitAttacked && stack.target instanceof Unit) {
       await System.show(stack, '＜ウィルス・攻＞', '基本BP+1000');
-      Effect.modifyBP(stack, stack.processing, stack.source, 1000, { isBaseBP: true });
+      Effect.modifyBP(stack, stack.processing, stack.target, 1000, { isBaseBP: true });
     }
   },
 };
