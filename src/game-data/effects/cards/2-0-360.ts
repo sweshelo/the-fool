fa8a040055cc9b08599afcfc9ca00113dda1df26
totoneko <32995138+totoneko@users.noreply.github.com>
2026-01-18 09:36:55 +0900

    Update 2-0-360.ts
    2-0-360.ts
    ・checkBattleの条件を削除
    ・自ユニットと相手ユニットの識別を実装
    
    fixes #235

--- src/game-data/effects/cards/2-0-360.ts
+++ src/game-data/effects/cards/2-0-360.ts
@@ -5,26 +5,24 @@ import type { CardEffects, StackWithCard } from '../schema/types';
 export const effects: CardEffects = {
   // ■不可侵光壁
   // あなたのユニットが戦闘した時
-  checkBattle: (stack: StackWithCard): boolean => {
-    return (
-      stack.source instanceof Unit &&
-      stack.source.owner.id === stack.processing.owner.id &&
-      stack.target instanceof Unit
-    );
+  checkBattle: (_stack: StackWithCard) => {
+    return true;
   },
 
   onBattle: async (stack: StackWithCard): Promise<void> => {
-    const attacker = stack.source;
-    const defender = stack.target;
+    if (!(stack.source instanceof Unit) || !(stack.target instanceof Unit)) return;
 
-    if (attacker instanceof Unit && defender instanceof Unit) {
-      const defenderBP = defender.currentBP;
-      await System.show(stack, '不可侵光壁', `BP+[戦闘中の敵ユニットのBP]`);
+    const ownUnit =
+      stack.processing.owner.id === stack.source.owner.id ? stack.source : stack.target;
+    const opponentUnit =
+      stack.processing.owner.id === stack.source.owner.id ? stack.target : stack.source;
 
-      Effect.modifyBP(stack, stack.processing, attacker, defenderBP, {
-        event: 'turnEnd',
-        count: 1,
-      });
-    }
+    const opponentBP = opponentUnit.currentBP;
+    await System.show(stack, '不可侵光壁', `BP+[戦闘中の敵ユニットのBP]`);
+
+    Effect.modifyBP(stack, stack.processing, ownUnit, opponentBP, {
+      event: 'turnEnd',
+      count: 1,
+    });
   },
 };
