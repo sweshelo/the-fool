e467306e3956c2812da39c2f08de2c0fdfeafc34
dalproc <username@users.noreply.github.com>
2026-01-20 18:58:30 +0900

    [炉皇女ヘスティア] カード効果修正

--- src/game-data/effects/cards/2-3-005.ts
+++ src/game-data/effects/cards/2-3-005.ts
@@ -2,41 +2,84 @@ import { Unit } from '@/package/core/class/card';
 import { Effect, EffectHelper, System } from '..';
 import type { CardEffects, StackWithCard } from '../schema/types';
 import master from '@/submodule/suit/catalog/catalog';
+import { Delta } from '@/package/core/class/delta';
 
 export const effects: CardEffects = {
   // 自身が召喚された時に発動する効果を記述
   onDriveSelf: async (stack: StackWithCard): Promise<void> => {
     if (EffectHelper.isUnitSelectable(stack.core, 'opponents', stack.processing.owner)) {
       await System.show(stack, 'ヘスティアのハピネスクッキング♪', '4000ダメージ');
-      const [target1] = await EffectHelper.pickUnit(
+      const [target] = await EffectHelper.pickUnit(
         stack,
         stack.processing.owner,
         'opponents',
         'ダメージを与えるユニットを選択して下さい',
         1
       );
-      const result1 = Effect.damage(stack, stack.processing, target1, 4000, 'effect');
+      Effect.damage(stack, stack.processing, target, 4000, 'effect');
+    }
+  },
 
-      const filter = (unit: Unit) =>
-        unit.id !== target1.id && unit.owner.id !== stack.processing.owner.id;
-      if (result1 && EffectHelper.isUnitSelectable(stack.core, filter, stack.processing.owner)) {
-        await System.show(stack, 'ヘスティアのハピネスクッキング♪', '3000ダメージ');
-        const [target2] = await EffectHelper.pickUnit(
-          stack,
-          stack.processing.owner,
-          filter,
-          'ダメージを与えるユニットを選択して下さい',
-          1
-        );
-        const result2 = Effect.damage(stack, stack.processing, target2, 3000, 'effect');
+  onBreak: async (stack: StackWithCard<Unit>): Promise<void> => {
+    // 自分の効果で破壊されたかチェック
+    if (
+      stack.source.id !== stack.processing.id ||
+      stack.option?.type !== 'break' ||
+      stack.option.cause !== 'damage'
+    )
+      return;
 
-        if (result2) {
-          await System.show(stack, 'ヘスティアのハピネスクッキング♪', '2000ダメージ');
-          stack.processing.owner.opponent.field.forEach(unit =>
-            Effect.damage(stack, stack.processing, unit, 2000, 'effect')
-          );
-        }
-      }
+    // 2回目の効果が発動しているかチェック
+    const effect2activated = stack.processing.delta.some(
+      delta =>
+        delta.source?.unit === stack.processing.id &&
+        delta.source.effectCode === 'ハピネスクッキング_2回目'
+    );
+    // 3回目の効果が発動しているかチェック
+    const effect3activated = stack.processing.delta.some(
+      delta =>
+        delta.source?.unit === stack.processing.id &&
+        delta.source.effectCode === 'ハピネスクッキング_3回目'
+    );
+
+    // 単体3000ダメージの効果
+    // 既に発動済み、または選択対象がない場合は発動しない
+    const filter = (unit: Unit) =>
+      unit.destination !== 'trash' && unit.owner.id !== stack.processing.owner.id;
+    if (
+      !effect2activated &&
+      EffectHelper.isUnitSelectable(stack.core, filter, stack.processing.owner)
+    ) {
+      await System.show(stack, 'ヘスティアのハピネスクッキング♪', '3000ダメージ');
+      const [target] = await EffectHelper.pickUnit(
+        stack,
+        stack.processing.owner,
+        filter,
+        'ダメージを与えるユニットを選択して下さい',
+        1
+      );
+      Effect.damage(stack, stack.processing, target, 3000, 'effect');
+      stack.processing.delta.push(
+        new Delta(
+          { type: 'banned' },
+          { source: { unit: stack.processing.id, effectCode: 'ハピネスクッキング_2回目' } }
+        )
+      );
+    }
+
+    // 全体2000ダメージの効果
+    // 既に発動済み、または敵ユニットがいない場合は発動しない
+    if (effect2activated && !effect3activated && stack.processing.owner.opponent.field.length > 0) {
+      await System.show(stack, 'ヘスティアのハピネスクッキング♪', '2000ダメージ');
+      stack.processing.owner.opponent.field.forEach(unit =>
+        Effect.damage(stack, stack.processing, unit, 2000, 'effect')
+      );
+      stack.processing.delta.push(
+        new Delta(
+          { type: 'banned' },
+          { source: { unit: stack.processing.id, effectCode: 'ハピネスクッキング_3回目' } }
+        )
+      );
     }
   },
 
