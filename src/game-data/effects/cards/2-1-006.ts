939bb2ffd4f34a70cf8e81c4e99d452fd733e02a
sweshelo <sweshelo@gmail.com>
2026-01-24 09:39:07 +0000

    ヤタガラス(2-1-006)のカード効果を実装

---
+++ src/game-data/effects/cards/2-1-006.ts
@@ -0,0 +1,27 @@
+import { Unit } from '@/package/core/class/card';
+import { Effect, System } from '..';
+import type { CardEffects, StackWithCard } from '../schema/types';
+import { PermanentEffect } from '../engine/permanent';
+
+export const effects: CardEffects = {
+  // 【加護】
+  onDriveSelf: async (stack: StackWithCard<Unit>): Promise<void> => {
+    await System.show(stack, '加護', '効果に選ばれない');
+    Effect.keyword(stack, stack.processing, stack.processing, '加護');
+  },
+
+  // ■サポーター／神獣
+  // あなたの【神獣】ユニットのBPを+1000する。
+  fieldEffect: (stack: StackWithCard<Unit>): void => {
+    PermanentEffect.mount(stack.processing, {
+      targets: ['owns'],
+      effect: (unit, source) => {
+        if (unit instanceof Unit) {
+          Effect.modifyBP(stack, stack.processing, unit, 1000, { source });
+        }
+      },
+      effectCode: 'サポーター／神獣',
+      condition: target => target.catalog.species?.includes('神獣') ?? false,
+    });
+  },
+};
