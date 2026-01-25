e4e3b991efa5322a6a8fd30c76739ef71d0ea2fa
sweshelo <sweshelo@gmail.com>
2026-01-25 08:45:54 +0000

    EffectHelper.combineを実装

--- src/game-data/effects/cards/2-2-020.ts
+++ src/game-data/effects/cards/2-2-020.ts
@@ -1,5 +1,5 @@
 import { Unit } from '@/package/core/class/card';
-import { Effect, EffectHelper, System } from '..';
+import { Effect, EffectHelper } from '..';
 import type { CardEffects, StackWithCard } from '../schema/types';
 import { Color } from '@/submodule/suit/constant/color';
 
@@ -9,18 +9,6 @@ const stealCP = (stack: StackWithCard<Unit>, self: Unit, owner: typeof self.owne
   const stolenCP = Math.min(12, opponent.cp.current);
   Effect.modifyCP(stack, self, opponent, -stolenCP);
   Effect.modifyCP(stack, self, owner, stolenCP);
-};
-
-// 連撃処理
-const comboEffect = async (stack: StackWithCard<Unit>, self: Unit, owner: typeof self.owner) => {
-  const [target] = await EffectHelper.pickUnit(
-    stack,
-    owner,
-    'opponents',
-    '基本BPを-2000するユニットを選択'
-  );
-  Effect.modifyBP(stack, self, target, -2000, { isBaseBP: true });
-  Effect.modifyBP(stack, self, self, 2000, { isBaseBP: true });
 };
 
 export const effects: CardEffects = {
@@ -48,23 +36,37 @@ export const effects: CardEffects = {
     const canCombo =
       hasUsedGreenCardThisTurn && EffectHelper.isUnitSelectable(stack.core, 'opponents', owner);
 
-    if (canStealCP && canCombo) {
-      // 両方発動
-      await System.show(
-        stack,
-        'ハートスティール＆連撃・バトレコンフュージョン',
-        'CP-12\nCP+[減少させたCP]\n基本BP-2000\n基本BP+2000'
-      );
-      stealCP(stack, self, owner);
-      await comboEffect(stack, self, owner);
-    } else if (canStealCP) {
-      // ハートスティールのみ
-      await System.show(stack, 'ハートスティール', 'CP-12\nCP+[減少させたCP]');
-      stealCP(stack, self, owner);
-    } else if (canCombo) {
-      // 連撃のみ
-      await System.show(stack, '連撃・バトレコンフュージョン', '基本BP-2000\n基本BP+2000');
-      await comboEffect(stack, self, owner);
-    }
+    await EffectHelper.combine(stack, [
+      {
+        title: 'ハートスティール',
+        description: 'CP-12\nCP+[減少させたCP]',
+        effect: () => stealCP(stack, self, owner),
+        condition: () => canStealCP,
+        order: 1,
+      },
+      {
+        title: '連撃・バトレコンフュージョン',
+        description: '基本BP-2000',
+        effect: async () => {
+          const [target] = await EffectHelper.pickUnit(
+            stack,
+            stack.processing.owner,
+            'opponents',
+            '基本BPを減少させるユニットを選択して下さい'
+          );
+          Effect.modifyBP(stack, stack.processing, target, -2000, { isBaseBP: true });
+        },
+        condition: () =>
+          EffectHelper.isUnitSelectable(stack.core, 'opponents', stack.processing.owner) &&
+          canCombo,
+      },
+      {
+        title: '連撃・バトレコンフュージョン',
+        description: '基本BP+2000',
+        effect: () =>
+          Effect.modifyBP(stack, stack.processing, stack.processing, 2000, { isBaseBP: true }),
+        condition: () => canCombo,
+      },
+    ]);
   },
 };
