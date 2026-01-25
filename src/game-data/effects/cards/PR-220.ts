00aa3ef9f650d4d56af0ea457c54762bbd0a5078
sweshelo <root@sweshelo.jp>
2026-01-25 18:51:04 +0900

    ロストドラゴンの残りの効果を実装

--- src/game-data/effects/cards/PR-220.ts
+++ src/game-data/effects/cards/PR-220.ts
@@ -1,8 +1,8 @@
-import type { Unit } from '@/package/core/class/card';
+import { Unit } from '@/package/core/class/card';
 import type { CardEffects, StackWithCard } from '../schema/types';
 import { EffectHelper } from '../engine/helper';
-import { System } from '../engine/system';
 import { Effect } from '../engine/effect';
+import { System } from '../engine/system';
 
 export const effects: CardEffects = {
   onDriveSelf: async (stack: StackWithCard<Unit>) => {
@@ -11,20 +11,55 @@ export const effects: CardEffects = {
       stack.processing.owner.opponent.field,
       ownTargets.length
     );
-    if (ownTargets.length === 0) return;
 
-    const message = [
-      '自身以外の味方全体を破壊',
-      opponentTargets.length >= 3
-        ? '相手ユニットをランダムで破壊\n2ライフダメージ'
-        : opponentTargets.length > 0
-          ? '相手ユニットをランダムで破壊\n1ライフダメージ'
-          : undefined,
-    ];
+    await EffectHelper.combine(stack, [
+      {
+        title: '滅セヨ、全テヲ',
+        description: '自身以外の味方全体を破壊',
+        effect: () => ownTargets.forEach(unit => Effect.break(stack, stack.processing, unit)),
+        condition: () => ownTargets.length > 0,
+      },
+      {
+        title: '滅セヨ、全テヲ',
+        description: '相手ユニットをランダムで破壊',
+        effect: () => opponentTargets.forEach(unit => Effect.break(stack, stack.processing, unit)),
+        condition: () => opponentTargets.length > 0,
+      },
+      {
+        title: '滅セヨ、全テヲ',
+        description: '1ライフダメージ',
+        effect: () => Effect.modifyLife(stack, stack.processing, stack.processing.owner, -1),
+        condition: () => opponentTargets.length < 3 && opponentTargets.length > 0,
+      },
+      {
+        title: '滅セヨ、全テヲ',
+        description: '2ライフダメージ',
+        effect: () => Effect.modifyLife(stack, stack.processing, stack.processing.owner, -2),
+        condition: () => opponentTargets.length >= 3,
+      },
+    ]);
+  },
 
-    await System.show(stack, '滅セヨ、全テヲ', message.join('\n'));
-    [...ownTargets, ...opponentTargets].forEach(unit =>
-      Effect.break(stack, stack.processing, unit)
-    );
+  onAttackSelf: async (stack: StackWithCard<Unit>) => {
+    const purple = stack.processing.owner.purple ?? 0;
+    if (purple > 0) {
+      await System.show(
+        stack,
+        '滅セヨ、全テヲ',
+        '全てのユニットの基本BPを[紫ゲージの数×1000]にする'
+      );
+      [stack.processing.owner, stack.processing.owner.opponent]
+        .flatMap(player => player.field)
+        .forEach(unit => {
+          Effect.modifyBP(stack, stack.processing, unit, purple * 1000 - unit.bp, {
+            isBaseBP: true,
+          });
+        });
+    }
+  },
+
+  onBreakSelf: async (stack: StackWithCard<Unit>) => {
+    await System.show(stack, '滅セヨ、全テヲ', '[タイニードラコ]を2枚作成');
+    EffectHelper.repeat(2, () => Effect.make(stack, stack.processing.owner, '2-0-027'));
   },
 };
