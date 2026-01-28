9b20fa70a181ddd6a8b99a9459057e8fa926cafc
dalproc <username@users.noreply.github.com>
2026-01-28 16:21:51 +0900

    カード効果修正

--- src/game-data/effects/cards/1-0-019.ts
+++ src/game-data/effects/cards/1-0-019.ts
@@ -1,24 +1,20 @@
 import type { Unit } from '@/package/core/class/card';
-import { Effect, System } from '..';
+import { Effect, EffectHelper, System } from '..';
 import type { CardEffects, StackWithCard } from '../schema/types';
 
 export const effects: CardEffects = {
   onDriveSelf: async (stack: StackWithCard<Unit>): Promise<void> => {
     const owner = stack.processing.owner;
-    const opponent = owner.opponent;
-    const isOnField = opponent.field.length > 0;
 
-    if (isOnField) {
+    if (EffectHelper.isUnitSelectable(stack.core, 'opponents', owner)) {
       await System.show(stack, 'ジャンプーダンス', '手札に戻す');
-      const [choice] = await System.prompt(stack, owner.id, {
-        title: '手札に戻すユニットを選択',
-        type: 'unit',
-        items: opponent.field,
-      });
-
-      const target = opponent.field.find(unit => unit.id === choice) ?? opponent.field[0];
-      if (!target) throw new Error('対戦相手のフィールドにユニットが存在しません');
-
+      const [target] = await EffectHelper.pickUnit(
+        stack,
+        owner,
+        'opponents',
+        '手札に戻すユニットを選択'
+      );
+      if (!target) return;
       Effect.bounce(stack, stack.processing, target, 'hand');
     }
   },
