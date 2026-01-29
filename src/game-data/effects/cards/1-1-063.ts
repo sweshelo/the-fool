73302ac127ee724fb944caf363c23364d3a16315
dalproc <username@users.noreply.github.com>
2026-01-30 01:01:04 +0900

    レビュー結果反映

--- src/game-data/effects/cards/1-1-063.ts
+++ src/game-data/effects/cards/1-1-063.ts
@@ -3,8 +3,12 @@ import type { CardEffects, StackWithCard } from '../schema/types';
 
 export const effects: CardEffects = {
   // カードが発動可能であるかを調べ、発動条件を満たしていれば true を、そうでなければ false を返す。
-  checkDrive: (_stack: StackWithCard) => {
-    return true;
+  checkDrive: (stack: StackWithCard) => {
+    const targets = [
+      ...stack.processing.owner.trigger.filter(card => card.id !== stack.processing.id),
+      ...stack.processing.owner.opponent.trigger,
+    ];
+    return targets.length > 0;
   },
 
   // 実際の効果本体
