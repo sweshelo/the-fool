ad4826301b2fb7159b44951b0b0051889251145c
sweshelo <sweshelo@gmail.com>
2026-01-30 21:48:27 +0900

    不正な返却値になるのを修正 combine条件修正

--- src/game-data/effects/engine/helper/combo.ts
+++ src/game-data/effects/engine/helper/combo.ts
@@ -16,9 +16,11 @@ export const GREEN_COMBO = (cost: number): ComboCheckCondition => ({
 
 export const helperCombo = (core: Core, condition: ComboCheckCondition) => {
   return core.histories.some(history => {
-    (condition.action ? condition.action === history.action : true) &&
-      (condition.cost ? condition.cost <= history.card.catalog.cost : true) &&
+    return (
+      (condition.action ? condition.action === history.action : true) &&
+      (condition.cost !== undefined ? condition.cost <= history.card.catalog.cost : true) &&
       (condition.type ? condition.type.includes(history.card.catalog.type) : true) &&
-      (condition.color ? condition.color.includes(history.card.catalog.color) : true);
+      (condition.color ? condition.color.includes(history.card.catalog.color) : true)
+    );
   });
 };
