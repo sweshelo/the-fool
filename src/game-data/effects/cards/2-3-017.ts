1aa9dde3c5255f2cd065155c3c01fe5a7ee26336
SenMiki <giichij2424@gmail.com>
2026-02-02 21:27:48 +0900

    2-3-017.tsの修正

--- src/game-data/effects/cards/2-3-017.ts
+++ src/game-data/effects/cards/2-3-017.ts
@@ -6,8 +6,8 @@ export const effects: CardEffects = {
   onDriveSelf: async (stack: StackWithCard<Unit>): Promise<void> => {
     const owner = stack.processing.owner;
 
-    // フィールドに空きがあるか（最大5体想定）
-    const hasFieldSpace = owner.field.length <= 4;
+    // フィールドに空きがあるか
+    const hasFieldSpace = owner.field.length < stack.core.room.rule.player.max.field;
 
     // デッキから条件に合うユニットを抽出
     const candidates = owner.deck.filter(
@@ -17,12 +17,8 @@ export const effects: CardEffects = {
         card.catalog.cost <= 1 // コスト1以下
     );
 
-    if (!hasFieldSpace) return;
-    await System.show(
-      stack,
-      '生殖',
-      'デッキから進化ユニット以外のコスト1以下の【昆虫】ユニットをランダムで1体【特殊召喚】'
-    );
+    if (!hasFieldSpace || candidates.length === 0) return;
+    await System.show(stack, '生殖', 'コスト1以下の【昆虫】ユニットをランダムで1体【特殊召喚】');
 
     const [target] = EffectHelper.random(candidates, 1);
     if (target instanceof Unit) {
