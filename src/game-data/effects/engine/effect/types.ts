e57d0e6c9ac7d5bd7d57195718cb5f08edd0df1a
sweshelo <sweshelo@gmail.com>
2026-03-03 01:06:44 +0900

    特殊条件による次元干渉を実装

--- src/game-data/effects/engine/effect/types.ts
+++ src/game-data/effects/engine/effect/types.ts
@@ -1,5 +1,6 @@
 import type { Delta, DeltaSource } from '@/package/core/class/delta';
 import type { GameEvent } from '../../schema/events';
+import type { Unit } from '@/package/core/class/card';
 
 export interface KeywordOptionParams {
   event?: GameEvent;
@@ -7,6 +8,13 @@ export interface KeywordOptionParams {
   cost?: number;
   onlyForOwnersTurn?: boolean;
   source?: DeltaSource;
+  /**
+   * 次元干渉の特殊条件関数
+   * @param self アタッカー
+   * @param blocker ブロッカーの候補
+   * @returns falseでブロック可能、trueでブロック不可能
+   */
+  condition?: (self?: Unit, blocker?: Unit) => boolean | unknown;
 }
 
 export type ModifyBPOption =
